9b34bb62bc950f50915c547af323d51191260713
jimMartin4321 <jamesrm4321@gmail.com>
2018-12-29 06:11:33 -0800

    removed the console.log statement in the api call helper function

--- server/apiCall.js
+++ server/apiCall.js
@@ -2,7 +2,6 @@ const axios = require('axios');
 const apiHost = 'https://api.iextrading.com/1.0';
 
 const apiCall = (symbol, res) => {
-  console.log(symbol);
   axios.get(`${apiHost}/stock/${symbol}/chart/1d?chartInterval=5`)
     .then(promise => promise)
     .then(response => {
